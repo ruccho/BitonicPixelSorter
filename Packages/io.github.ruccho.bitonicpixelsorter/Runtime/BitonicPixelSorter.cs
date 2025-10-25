904f0803b620095f42e75cf67b85846ece9a392f
ruccho <noboroo2000@yahoo.co.jp>
2025-10-26 00:27:11 +0900

    reformat

--- Packages/io.github.ruccho.bitonicpixelsorter/Runtime/BitonicPixelSorter.cs
+++ Packages/io.github.ruccho.bitonicpixelsorter/Runtime/BitonicPixelSorter.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Object = UnityEngine.Object;
 
 namespace Ruccho.Utilities
@@ -9,29 +6,43 @@ namespace Ruccho.Utilities
     public class BitonicPixelSorter : MonoBehaviour
     {
         [SerializeField] private bool useAsImageEffect = true;
-        [SerializeField] private ComputeShader shader = default;
+        [SerializeField] private ComputeShader shader;
         [SerializeField] private bool direction = true;
         [SerializeField] private bool ascending = true;
-        [SerializeField, Range(0f, 1f)] private float thresholdMin = 0.4f;
-        [SerializeField, Range(0f, 1f)] private float thresholdMax = 0.6f;
-
-        private RenderTexture metaTex = default;
-        private RenderTexture sortTex = default;
-
-        private int metaPassIndex = default;
-        private int sortPassIndex = default;
+        [SerializeField] [Range(0f, 1f)] private float thresholdMin = 0.4f;
+        [SerializeField] [Range(0f, 1f)] private float thresholdMax = 0.6f;
 
         private readonly int k_direction = Shader.PropertyToID("direction");
-        private readonly int k_thresholdMin = Shader.PropertyToID("thresholdMin");
-        private readonly int k_thresholdMax = Shader.PropertyToID("thresholdMax");
-        private readonly int k_srcTex = Shader.PropertyToID("srcTex");
-        private readonly int k_metaTex = Shader.PropertyToID("metaTex");
-        private readonly int k_srcMetaTex = Shader.PropertyToID("srcMetaTex");
-        private readonly int k_sortTex = Shader.PropertyToID("sortTex");
-        private readonly int k_ordering = Shader.PropertyToID("ordering");
         private readonly int k_MaxLevels = Shader.PropertyToID("maxLevels");
+        private readonly int k_metaTex = Shader.PropertyToID("metaTex");
+        private readonly int k_ordering = Shader.PropertyToID("ordering");
+        private readonly int k_sortTex = Shader.PropertyToID("sortTex");
+        private readonly int k_srcMetaTex = Shader.PropertyToID("srcMetaTex");
+        private readonly int k_srcTex = Shader.PropertyToID("srcTex");
+        private readonly int k_thresholdMax = Shader.PropertyToID("thresholdMax");
+        private readonly int k_thresholdMin = Shader.PropertyToID("thresholdMin");
 
-        private bool isInitialized = false;
+        private bool isInitialized;
+
+        private int metaPassIndex;
+
+        private RenderTexture metaTex;
+        private int sortPassIndex;
+        private RenderTexture sortTex;
+
+        private void OnDisable()
+        {
+            EnsureDestroyed(ref sortTex);
+            EnsureDestroyed(ref metaTex);
+        }
+
+        private void OnRenderImage(RenderTexture src, RenderTexture dest)
+        {
+            if (useAsImageEffect)
+                Execute(src, dest);
+            else
+                Graphics.Blit(src, dest);
+        }
 
         private void Initialize()
         {
@@ -40,18 +51,6 @@ namespace Ruccho.Utilities
 
             metaPassIndex = shader.FindKernel("MetaPass");
             sortPassIndex = shader.FindKernel("SortPass");
-        }
-
-        private void OnRenderImage(RenderTexture src, RenderTexture dest)
-        {
-            if (useAsImageEffect)
-            {
-                Execute(src, dest);
-            }
-            else
-            {
-                Graphics.Blit(src, dest);
-            }
         }
 
         public void Execute(Texture src, RenderTexture dst)
@@ -64,13 +63,13 @@ namespace Ruccho.Utilities
 
             Initialize();
 
-            int width = src.width;
-            int height = src.height;
-            int size = direction ? width : height;
-            int lines = direction ? height : width;
+            var width = src.width;
+            var height = src.height;
+            var size = direction ? width : height;
+            var lines = direction ? height : width;
 
-            int metaWidth = direction ? width / 2 : width;
-            int metaHeight = direction ? height : height / 2;
+            var metaWidth = direction ? width / 2 : width;
+            var metaHeight = direction ? height : height / 2;
 
             if (size >= 2048)
             {
@@ -132,24 +131,14 @@ namespace Ruccho.Utilities
             }
         }
 
-        private void OnDisable()
-        {
-            EnsureDestroyed(ref sortTex);
-            EnsureDestroyed(ref metaTex);
-        }
-
         private static void EnsureDestroyed<T>(ref T obj) where T : Object
         {
             if (obj)
             {
                 if (Application.isPlaying)
-                {
                     Destroy(obj);
-                }
                 else
-                {
                     DestroyImmediate(obj);
-                }
             }
 
             obj = null;
