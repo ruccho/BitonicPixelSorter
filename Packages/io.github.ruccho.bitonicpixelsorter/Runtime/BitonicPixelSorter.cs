62a81a4a433fa6438b78fe66cdb8dbb9ab11474e
ruccho <noboroo2000@yahoo.co.jp>
2025-10-25 23:41:47 +0900

    Improve occupancy

--- Packages/io.github.ruccho.bitonicpixelsorter/Runtime/BitonicPixelSorter.cs
+++ Packages/io.github.ruccho.bitonicpixelsorter/Runtime/BitonicPixelSorter.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace Ruccho.Utilities
 {
@@ -36,11 +37,11 @@ namespace Ruccho.Utilities
         {
             if (isInitialized) return;
             isInitialized = true;
-            
+
             metaPassIndex = shader.FindKernel("MetaPass");
             sortPassIndex = shader.FindKernel("SortPass");
         }
-        
+
         private void OnRenderImage(RenderTexture src, RenderTexture dest)
         {
             if (useAsImageEffect)
@@ -55,8 +56,14 @@ namespace Ruccho.Utilities
 
         public void Execute(Texture src, RenderTexture dst)
         {
+            if (!isActiveAndEnabled)
+            {
+                Graphics.Blit(src, dst);
+                return;
+            }
+
             Initialize();
-            
+
             int width = src.width;
             int height = src.height;
             int size = direction ? width : height;
@@ -72,46 +79,80 @@ namespace Ruccho.Utilities
                 return;
             }
 
-            EnsureBufferTextureSize(ref metaTex, metaWidth, metaHeight, RenderTextureFormat.RGFloat);
+            EnsureBufferTextureSize(ref metaTex, metaWidth, metaHeight, RenderTextureFormat.RInt);
             EnsureBufferTextureSize(ref sortTex, width, height, RenderTextureFormat.ARGB32);
 
             shader.SetBool(k_direction, direction);
             shader.SetFloat(k_thresholdMin, thresholdMin);
             shader.SetFloat(k_thresholdMax, thresholdMax);
-            
+
             shader.SetTexture(metaPassIndex, k_srcTex, src);
             shader.SetTexture(metaPassIndex, k_metaTex, metaTex);
-            
-            shader.GetKernelThreadGroupSizes(metaPassIndex, out uint metaGroupX, out uint metaGroupY, out uint metaGroupZ);
-            uint metaGroupSize = metaGroupX * metaGroupY * metaGroupZ;
-            int metaDispatchCount = Mathf.CeilToInt((float)lines * 2 / metaGroupSize);
-            
+
+            shader.GetKernelThreadGroupSizes(metaPassIndex, out var metaGroupX, out var metaGroupY,
+                out var metaGroupZ);
+            var metaGroupSize = metaGroupX * metaGroupY * metaGroupZ;
+            var metaDispatchCount = Mathf.CeilToInt((float)lines * 2 / metaGroupSize);
+
             shader.Dispatch(metaPassIndex, metaDispatchCount, 1, 1);
-            
+
             Graphics.Blit(src, sortTex);
-            
+
             shader.SetTexture(sortPassIndex, k_srcMetaTex, metaTex);
             shader.SetTexture(sortPassIndex, k_sortTex, sortTex);
             shader.SetBool(k_ordering, ascending);
 
-            int maxLevel = Mathf.CeilToInt(Mathf.Log(size, 2));
-            
+            var maxLevel = Mathf.CeilToInt(Mathf.Log(size, 2));
+
             shader.SetInt(k_MaxLevels, maxLevel);
-            
+
             shader.Dispatch(sortPassIndex, lines, 1, 1);
-            
+
             Graphics.Blit(sortTex, dst);
         }
 
-        private static void EnsureBufferTextureSize(ref RenderTexture tex, int width, int height, RenderTextureFormat format)
+        private static void EnsureBufferTextureSize(ref RenderTexture tex, int width, int height,
+            RenderTextureFormat format)
         {
-            if (!tex || tex.width != width || tex.height != height)
+            if (!tex)
             {
                 tex = new RenderTexture(width, height, 0, format);
                 tex.enableRandomWrite = true;
                 tex.Create();
                 Debug.Log("[BitonicPixelSorter] RenderTexture created.");
             }
+            else if (tex.width != width || tex.height != height)
+            {
+                tex.Release();
+                tex.width = width;
+                tex.height = height;
+                tex.format = format;
+                tex.Create();
+                Debug.Log("[BitonicPixelSorter] RenderTexture created.");
+            }
+        }
+
+        private void OnDisable()
+        {
+            EnsureDestroyed(ref sortTex);
+            EnsureDestroyed(ref metaTex);
+        }
+
+        private static void EnsureDestroyed<T>(ref T obj) where T : Object
+        {
+            if (obj)
+            {
+                if (Application.isPlaying)
+                {
+                    Destroy(obj);
+                }
+                else
+                {
+                    DestroyImmediate(obj);
+                }
+            }
+
+            obj = null;
         }
     }
 }
